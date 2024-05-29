d7839b3ea87fabad6179fcdfc7331cf3d93d9268
Sunil Rajput <48656178+sreiker@users.noreply.github.com>
2024-05-29 14:36:10 +0530

    Update iOSApp.java

--- src/main/java/iOSApp.java
+++ src/main/java/iOSApp.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.ScreenOrientation;
 
 import java.net.URL;
 import java.util.HashMap;
