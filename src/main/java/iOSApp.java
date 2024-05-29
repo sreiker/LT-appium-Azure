f22c2f95c53252d469f7dd1019072160e66f6f22
Sunil Rajput <48656178+sreiker@users.noreply.github.com>
2024-05-29 14:25:18 +0530

    Update iOSApp.java

--- src/main/java/iOSApp.java
+++ src/main/java/iOSApp.java
@@ -43,15 +43,49 @@ String file=System.getenv("LT_BUILD_NAME");
             //capabilities.setCapability("geoLocation", "HK");
 
             DesiredCapabilities capabilities = new DesiredCapabilities();
-HashMap<String, Object> ltOptions = new HashMap<String, Object>();
-ltOptions.put("w3c", true);
-ltOptions.put("build", file);
-ltOptions.put("platformName", "ios");
-ltOptions.put("deviceName", "iPhone 15");
-ltOptions.put("platformVersion", "17");
-ltOptions.put("app", "lt://APP1016053601716969413336816");
-ltOptions.put("isRealMobile", true);
-capabilities.setCapability("lt:options", ltOptions);
+// HashMap<String, Object> ltOptions = new HashMap<String, Object>();
+// ltOptions.put("w3c", true);
+// ltOptions.put("build", file);
+// ltOptions.put("platformName", "ios");
+// ltOptions.put("deviceName", "iPhone 15");
+// ltOptions.put("platformVersion", "17");
+// ltOptions.put("app", "lt://APP1016053601716969413336816");
+// ltOptions.put("isRealMobile", true);
+// capabilities.setCapability("lt:options", ltOptions);
+
+
+capabilities.setCapability("app", "lt://APP1016053601716969413336816");
+capabilities.setCapability("appiumVersion", "latest");
+capabilities.setCapability("build", "Azure-lambdatest-Greystar-DevOps-66057");
+capabilities.setCapability("console", false);
+capabilities.setCapability("deviceName", "iPhone 13 Mini");
+capabilities.setCapability("device_viewport", "390 x 844");
+capabilities.setCapability("devicelog", true);
+capabilities.setCapability("enableNetworkThrottling", false);
+capabilities.setCapability("extendedDebuging", true);
+capabilities.setCapability("headless", false);
+capabilities.setCapability("idleTimeout", 240);
+capabilities.setCapability("isAppAutomate", true);
+capabilities.setCapability("isRealMobile", true);
+capabilities.setCapability("name", "SignUp-Sanity-7775026232-d1b3b0b5a80256fc626e80d313c2e09cc06d3e3d");
+capabilities.setCapability("nativeWebScreenshot", true);
+capabilities.setCapability("network", true);
+capabilities.setCapability("newCommandTimeout", "0");
+capabilities.setCapability("performance", false);
+capabilities.setCapability("platformName", "ios");
+capabilities.setCapability("platformVersion", "15");
+capabilities.setCapability("printPageSourceOnFindFailure", true);
+capabilities.setCapability("project", "Resident.AcptTest");
+capabilities.setCapability("region", "us");
+capabilities.setCapability("resolution", "1080 x 2340 px");
+capabilities.setCapability("tunnel", false);
+capabilities.setCapability("tunnelIdentifier", "");
+capabilities.setCapability("userAgent", "selenium/3.141.0 (.net windows)");
+capabilities.setCapability("version", "");
+capabilities.setCapability("video", true);
+capabilities.setCapability("visual", true);
+capabilities.setCapability("w3c", false);
+capabilities.setCapability("queueTimeout", 600);
 
             String hub = "http://" + "sunilr" + ":" + "LtQPqIWXiN4kHjRSQ9juzDmQZV7tpbssbvyEJ13JPS4QfhGduU" + "@" + "mobile-hub.lambdatest.com" + "/wd/hub";
             
